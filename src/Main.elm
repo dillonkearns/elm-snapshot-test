bb49423debcffe2aeb19a6716e3403913b54fa44
Dillon Kearns <dillon@dillonkearns.com>
2020-06-17 19:38:02 -0700

    Add jest dependency and initial main module.

---
+++ src/Main.elm
@@ -0,0 +1,23 @@
+port module Main exposing (..)
+
+
+port snapshot : { name : String, value : String } -> Cmd msg
+
+
+type Model
+    = Model
+
+
+main =
+    Platform.worker
+        { init =
+            \() ->
+                ( Model
+                , snapshot
+                    { name = "example1"
+                    , value = "abcdefg"
+                    }
+                )
+        , update = \msg model -> ( model, Cmd.none )
+        , subscriptions = \model -> Sub.none
+        }
