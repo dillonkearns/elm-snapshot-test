42c8047b9cfc5a6b0d39450c758474099c9fc02b
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 07:48:47 -0700

    Update for gilded rose starting point.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 port module Main exposing (..)
 
+import GildedRose
 import Json.Encode as Encode
 import Permutations
 
@@ -7,67 +8,20 @@ import Permutations
 port snapshot : { name : String, value : Encode.Value } -> Cmd msg
 
 
-type Model
-    = Model
-
-
-init : Int
-init =
-    0
-
-
-type alias Game =
-    ( Int, Int )
-
-
-score : Game -> String
-score ( player1, player2 ) =
-    if player1 > 3 then
-        if player1 > (player2 + 1) then
-            "Player 1 wins!"
-
-        else
-            "Ad player 1"
-
-    else if player1 == player2 then
-        playerScoreToString player1 ++ " all"
-
-    else
-        playerScoreToString player1
-            ++ "-"
-            ++ playerScoreToString player2
-
-
-playerScoreToString : Int -> String
-playerScoreToString playerScore =
-    case playerScore of
-        0 ->
-            "Love"
-
-        1 ->
-            "15"
-
-        2 ->
-            "30"
-
-        3 ->
-            "40"
-
-        _ ->
-            "Unhandled"
-
-
 main =
     Platform.worker
         { init =
             \() ->
-                ( Model
+                ( ()
                 , snapshot
                     { name = "example1"
-                    , value =
-                        Permutations.approve2 (\a b -> score ( a, b )) [ 0, 1, 2, 3, 4, 5 ] [ 0, 1, 2, 3, 4, 5 ]
+                    , value = doPermutations ()
                     }
                 )
         , update = \msg model -> ( model, Cmd.none )
         , subscriptions = \model -> Sub.none
         }
+
+
+doPermutations () =
+    Permutations.approve3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ]) [] [] []
--- src/Permutations.elm
+++ src/Permutations.elm
@@ -19,6 +19,19 @@ map2 function list1 list2 =
         list1
 
 
+map3 : (a -> b -> c -> d) -> List a -> List b -> List c -> List d
+map3 function list1 list2 list3 =
+    List.concatMap
+        (\item1 ->
+            List.concatMap
+                (\item2 ->
+                    List.map (function item1 item2) list3
+                )
+                list2
+        )
+        list1
+
+
 approve2 : (a -> b -> c) -> List a -> List b -> Encode.Value
 approve2 function list1 list2 =
     map2
@@ -29,4 +42,18 @@ approve2 function list1 list2 =
         )
         list1
         list2
+        |> Encode.object
+
+
+approve3 : (a -> b -> c -> d) -> List a -> List b -> List c -> Encode.Value
+approve3 function list1 list2 list3 =
+    map3
+        (\a b c ->
+            ( Debug.toString a ++ ", " ++ Debug.toString b ++ ", " ++ Debug.toString c
+            , function a b c |> Debug.toString |> Encode.string
+            )
+        )
+        list1
+        list2
+        list3
         |> Encode.object
