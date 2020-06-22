99938d9f6219f43e9ca8da87fd3d96ccb666a8ae
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 10:11:11 -0700

    Reformat.

--- src/Main.elm
+++ src/Main.elm
@@ -24,4 +24,7 @@ main =
 
 
 doPermutations () =
-    Permutations.approve3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ]) [ "Aged Brie" ] [ 0 ] [ 0 ]
+    Permutations.approve3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
+        [ "Aged Brie" ]
+        [ 0 ]
+        [ 0 ]
