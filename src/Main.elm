eb7fd1c4fb2749288d7c89000df7f517e6545c19
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 14:02:30 -0700

    Add test case.

--- src/Main.elm
+++ src/Main.elm
@@ -27,4 +27,4 @@ doPermutations () =
     Permutations.approve3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
         [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Not a special item", "Backstage passes to a TAFKAL80ETC concert" ]
         [ -1, 0, 6, 11, 12, 13 ]
-        [ 0, 1, 48, 49, 50 ]
+        [ 0, 1, 48, 49, 50, 51 ]
