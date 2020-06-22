4de30520d80c44b576c17f00903bb3525703c30f
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 14:04:49 -0700

    Remove dead code.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -98,26 +98,19 @@ updateItemQuality_ ((Item name sellIn quality) as item) =
                     sellIn
         in
         if sellIn_ < 0 then
-            if name /= "Aged Brie" then
-                if name /= "Backstage passes to a TAFKAL80ETC concert" then
-                    if quality_ > 0 then
-                        if name /= "Sulfuras, Hand of Ragnaros" then
-                            Item name sellIn_ (quality_ - 1)
-
-                        else
-                            Item name sellIn_ quality_
+            if name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if quality_ > 0 then
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        Item name sellIn_ (quality_ - 1)
 
                     else
                         Item name sellIn_ quality_
 
                 else
-                    Item name sellIn_ (quality_ - quality_)
-
-            else if quality_ < 50 then
-                Item name sellIn_ (quality_ + 1)
+                    Item name sellIn_ quality_
 
             else
-                Item name sellIn_ quality_
+                Item name sellIn_ (quality_ - quality_)
 
         else
             Item name sellIn_ quality_
