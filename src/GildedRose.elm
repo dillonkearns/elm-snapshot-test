d28fbbe4fc95089b67ba21037a3608135e4e3c4d
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 13:41:16 -0700

    Remove and conditional that always fails.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -19,7 +19,7 @@ updateItemQuality_ ((Item name sellIn quality) as item) =
     if name == "Aged Brie" then
         let
             quality_ =
-                if False && name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if False then
                     if quality > 0 then
                         if name /= "Sulfuras, Hand of Ragnaros" then
                             quality - 1
