ee1f20211b2892ca67eeb3921b8b2f01292f76e3
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 19:52:05 -0700

    Add new file.

---
+++ src/Item.elm
@@ -0,0 +1,114 @@
+module Item exposing (..)
+
+
+type Item
+    = Item String Int Int
+
+
+updateItemQuality_ : Item -> Item
+updateItemQuality_ ((Item name sellIn quality) as item) =
+    case name of
+        "Aged Brie" ->
+            let
+                quality_ =
+                    if quality < 50 then
+                        quality + 1
+
+                    else
+                        quality
+
+                sellIn_ =
+                    sellIn - 1
+            in
+            if sellIn_ < 0 then
+                if quality_ < 50 then
+                    Item name sellIn_ (quality_ + 1)
+
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ quality_
+
+        "Backstage passes to a TAFKAL80ETC concert" ->
+            let
+                quality_ =
+                    if quality < 50 then
+                        quality
+                            + 1
+                            + (if sellIn < 11 then
+                                if quality < 49 then
+                                    1
+                                        + (if sellIn < 6 then
+                                            if quality < 48 then
+                                                1
+
+                                            else
+                                                0
+
+                                           else
+                                            0
+                                          )
+
+                                else
+                                    0
+
+                               else
+                                0
+                              )
+
+                    else
+                        quality
+
+                sellIn_ =
+                    sellIn - 1
+            in
+            if sellIn_ < 0 then
+                Item name sellIn_ (quality_ - quality_)
+
+            else
+                Item name sellIn_ quality_
+
+        "Sulfuras, Hand of Ragnaros" ->
+            let
+                quality_ =
+                    if quality > 0 then
+                        quality
+
+                    else
+                        quality
+
+                sellIn_ =
+                    sellIn
+            in
+            if sellIn_ < 0 then
+                if quality_ > 0 then
+                    Item name sellIn_ quality_
+
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ quality_
+
+        _ ->
+            let
+                quality_ =
+                    if quality > 0 then
+                        quality - 1
+
+                    else
+                        quality
+
+                sellIn_ =
+                    sellIn - 1
+            in
+            if sellIn_ < 0 then
+                if quality_ > 0 then
+                    Item name sellIn_ (quality_ - 1)
+
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ quality_
