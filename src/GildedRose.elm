20d408af49ed318b89f3b4253e9e2930523936ad
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 19:44:31 -0700

    Change to case statement.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -16,91 +16,22 @@ updateQuality =
 
 updateItemQuality_ : Item -> Item
 updateItemQuality_ ((Item name sellIn quality) as item) =
-    if name == "Aged Brie" then
-        let
-            quality_ =
-                if quality < 50 then
-                    quality + 1
-
-                else
-                    quality
-
-            sellIn_ =
-                sellIn - 1
-        in
-        if sellIn_ < 0 then
-            if quality_ < 50 then
-                Item name sellIn_ (quality_ + 1)
-
-            else
-                Item name sellIn_ quality_
-
-        else
-            Item name sellIn_ quality_
-
-    else if name == "Backstage passes to a TAFKAL80ETC concert" then
-        let
-            quality_ =
-                if quality < 50 then
-                    quality
-                        + 1
-                        + (if sellIn < 11 then
-                            if quality < 49 then
-                                1
-                                    + (if sellIn < 6 then
-                                        if quality < 48 then
-                                            1
-
-                                        else
-                                            0
-
-                                       else
-                                        0
-                                      )
-
-                            else
-                                0
-
-                           else
-                            0
-                          )
-
-                else
-                    quality
-
-            sellIn_ =
-                sellIn - 1
-        in
-        if sellIn_ < 0 then
-            Item name sellIn_ (quality_ - quality_)
-
-        else
-            Item name sellIn_ quality_
-
-    else
-        let
-            quality_ =
-                if quality > 0 then
-                    if name /= "Sulfuras, Hand of Ragnaros" then
-                        quality - 1
+    case name of
+        "Aged Brie" ->
+            let
+                quality_ =
+                    if quality < 50 then
+                        quality + 1
 
                     else
                         quality
 
-                else
-                    quality
-
-            sellIn_ =
-                if name /= "Sulfuras, Hand of Ragnaros" then
+                sellIn_ =
                     sellIn - 1
-
-                else
-                    sellIn
-        in
-        if sellIn_ < 0 then
-            if quality_ > 0 then
-                if name /= "Sulfuras, Hand of Ragnaros" then
-                    Item name sellIn_ (quality_ - 1)
+            in
+            if sellIn_ < 0 then
+                if quality_ < 50 then
+                    Item name sellIn_ (quality_ + 1)
 
                 else
                     Item name sellIn_ quality_
@@ -108,5 +39,75 @@ updateItemQuality_ ((Item name sellIn quality) as item) =
             else
                 Item name sellIn_ quality_
 
-        else
-            Item name sellIn_ quality_
+        "Backstage passes to a TAFKAL80ETC concert" ->
+            let
+                quality_ =
+                    if quality < 50 then
+                        quality
+                            + 1
+                            + (if sellIn < 11 then
+                                if quality < 49 then
+                                    1
+                                        + (if sellIn < 6 then
+                                            if quality < 48 then
+                                                1
+
+                                            else
+                                                0
+
+                                           else
+                                            0
+                                          )
+
+                                else
+                                    0
+
+                               else
+                                0
+                              )
+
+                    else
+                        quality
+
+                sellIn_ =
+                    sellIn - 1
+            in
+            if sellIn_ < 0 then
+                Item name sellIn_ (quality_ - quality_)
+
+            else
+                Item name sellIn_ quality_
+
+        _ ->
+            let
+                quality_ =
+                    if quality > 0 then
+                        if name /= "Sulfuras, Hand of Ragnaros" then
+                            quality - 1
+
+                        else
+                            quality
+
+                    else
+                        quality
+
+                sellIn_ =
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        sellIn - 1
+
+                    else
+                        sellIn
+            in
+            if sellIn_ < 0 then
+                if quality_ > 0 then
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        Item name sellIn_ (quality_ - 1)
+
+                    else
+                        Item name sellIn_ quality_
+
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ quality_
