ab7b07762f52c79e05064e292d3ec17aa97f1475
Dillon Kearns <dillon@dillonkearns.com>
2020-06-22 20:07:36 -0700

    Use record update.

--- src/Item.elm
+++ src/Item.elm
@@ -25,7 +25,7 @@ updateItemQuality_ ({ name, sellIn, quality } as item) =
             in
             if sellIn_ < 0 then
                 if quality_ < 50 then
-                    Item name sellIn_ (quality_ + 1)
+                    { item | sellIn = sellIn_, quality = quality_ + 1 }
 
                 else
                     { item | sellIn = sellIn_, quality = quality_ }
