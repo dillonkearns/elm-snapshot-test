9686c2a4123a8b4179751e5300088293db2c58a5
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 14:12:53 -0700

    Add test case.

--- src/SnapshotTests.elm
+++ src/SnapshotTests.elm
@@ -9,7 +9,7 @@ all =
         \() ->
             Permutations.verifyCombinations3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
                 [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Not a special item", "Backstage passes to a TAFKAL80ETC concert" ]
-                [ -1, 0, 6, 11, 12, 13, 14, 15 ]
+                [ -1, 0, 6, 11, 12, 13, 14, 15, 16 ]
                 [ 0, 1, 48, 49, 50, 51 ]
     , test "example2" <|
         \() ->
