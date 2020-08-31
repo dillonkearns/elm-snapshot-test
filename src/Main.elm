07a89076c18e48e380cbfb95f85595d714f6d2b6
Dillon Kearns <dillon@dillonkearns.com>
2020-08-30 21:32:16 -0700

    Create wrapper function for multiple test cases.

--- src/Main.elm
+++ src/Main.elm
@@ -13,18 +13,25 @@ main =
         { init =
             \() ->
                 ( ()
-                , snapshot
-                    { name = "example1"
-                    , value = doPermutations ()
-                    }
+                , Cmd.batch all
                 )
         , update = \msg model -> ( model, Cmd.none )
         , subscriptions = \model -> Sub.none
         }
 
 
-doPermutations () =
-    Permutations.verifyCombinations3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
-        [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Not a special item", "Backstage passes to a TAFKAL80ETC concert" ]
-        [ -1, 0, 6, 11, 12, 13, 14, 15 ]
-        [ 0, 1, 48, 49, 50, 51 ]
+all =
+    [ test "example1" <|
+        \() ->
+            Permutations.verifyCombinations3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
+                [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Not a special item", "Backstage passes to a TAFKAL80ETC concert" ]
+                [ -1, 0, 6, 11, 12, 13, 14, 15 ]
+                [ 0, 1, 48, 49, 50, 51 ]
+    ]
+
+
+test name getVerification =
+    snapshot
+        { name = name
+        , value = getVerification ()
+        }
