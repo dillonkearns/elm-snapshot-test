7c488dc8e7bab83bbce6f901e33c9cd496935bab
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 14:33:16 -0700

    Clean slate.

--- src/SnapshotTests.elm
+++ src/SnapshotTests.elm
@@ -5,16 +5,4 @@ import Permutations exposing (test)
 
 
 all =
-    [ test "example1" <|
-        \() ->
-            Permutations.verifyCombinations3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
-                [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Not a special item", "Backstage passes to a TAFKAL80ETC concert" ]
-                [ -1, 0, 6, 11, 12, 13, 14, 15, 16 ]
-                [ 0, 1, 48, 49, 50, 51 ]
-    , test "example2" <|
-        \() ->
-            Permutations.verify3 (\a b c -> GildedRose.updateQuality [ GildedRose.Item a b c ])
-                ""
-                0
-                0
-    ]
+    []
