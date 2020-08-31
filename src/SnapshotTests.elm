e46ac9ffaa85497f230824db55cca1dc4608ec50
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 15:41:33 -0700

    Extract param.

--- src/SnapshotTests.elm
+++ src/SnapshotTests.elm
@@ -8,12 +8,12 @@ all =
     [ test "example" <|
         \() ->
             Permutations.verifyCombinations3
-                (\n s quality ->
+                (\n s q ->
                     let
-                        createItem name sellIn =
+                        createItem name sellIn quality =
                             GildedRose.Item name sellIn quality
                     in
-                    GildedRose.updateQuality [ createItem n s ]
+                    GildedRose.updateQuality [ createItem n s q ]
                 )
                 [ "", "Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" ]
                 (List.range -1 50)
