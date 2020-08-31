b35718c73e939347a84a5ad56236ca4a215b3ad7
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 13:38:22 -0700

    Revert GildedRose setup.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -1,7 +1,5 @@
 module GildedRose exposing (Item(..), updateQuality)
 
-import Item
-
 
 type alias GildedRose =
     List Item
@@ -11,22 +9,84 @@ type Item
     = Item String Int Int
 
 
-updateQuality : List Item -> List Item
+updateQuality : GildedRose -> GildedRose
 updateQuality =
+    List.map updateQualityItem
+
+
+updateQualityItem (Item name sellIn quality) =
     let
-        _ =
-            125
+        quality_ =
+            if name /= "Aged Brie" && name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if quality > 0 then
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        quality - 1
+
+                    else
+                        quality
+
+                else
+                    quality
+
+            else if quality < 50 then
+                quality
+                    + 1
+                    + (if name == "Backstage passes to a TAFKAL80ETC concert" then
+                        if sellIn < 11 then
+                            if quality < 49 then
+                                1
+                                    + (if sellIn < 6 then
+                                        if quality < 48 then
+                                            1
+
+                                        else
+                                            0
+
+                                       else
+                                        0
+                                      )
+
+                            else
+                                0
+
+                        else
+                            0
+
+                       else
+                        0
+                      )
+
+            else
+                quality
+
+        sellIn_ =
+            if name /= "Sulfuras, Hand of Ragnaros" then
+                sellIn - 1
+
+            else
+                sellIn
     in
-    List.map updateItemQuality_
+    if sellIn_ < 0 then
+        if name /= "Aged Brie" then
+            if name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if quality_ > 0 then
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        Item name sellIn_ (quality_ - 1)
 
+                    else
+                        Item name sellIn_ quality_
 
-updateItemQuality_ : Item -> Item
-updateItemQuality_ ((Item name sellIn quality) as item) =
-    Item.updateItemQuality_
-        { name = name
-        , sellIn = sellIn
-        , quality = quality
-        }
-        |> (\updatedItem ->
-                Item updatedItem.name updatedItem.sellIn updatedItem.quality
-           )
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ (quality_ - quality_)
+
+        else if quality_ < 50 then
+            Item name sellIn_ (quality_ + 1)
+
+        else
+            Item name sellIn_ quality_
+
+    else
+        Item name sellIn_ quality_
