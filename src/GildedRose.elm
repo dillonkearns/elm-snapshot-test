29e044863f2ff17ae7e6ec7f554666fe8b792af7
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 15:56:54 -0700

    Use createName in createItem.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -25,7 +25,7 @@ createName name =
 
 createItem : String -> Int -> Int -> Item
 createItem name sellIn quality =
-    Item name sellIn quality
+    Item (createName name) sellIn quality
 
 
 updateQuality : GildedRose -> GildedRose
