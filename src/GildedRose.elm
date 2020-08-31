ba73e9738c8296ea544e8f7ff47a1636838364d4
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 16:39:45 -0700

    Collapse if.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -76,12 +76,8 @@ updateQualityItem ((Item (Name name) (SellIn sellIn) (Quality quality)) as item)
                         if sellIn < 11 then
                             if quality < 49 then
                                 1
-                                    + (if sellIn < 6 then
-                                        if quality < 48 then
-                                            1
-
-                                        else
-                                            0
+                                    + (if sellIn < 6 && quality < 48 then
+                                        1
 
                                        else
                                         0
