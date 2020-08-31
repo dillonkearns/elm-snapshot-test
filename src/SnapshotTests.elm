59728249ce9f6e2925a36cb82dad8dfd5e59e34f
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 15:11:39 -0700

    Reformat.

--- src/SnapshotTests.elm
+++ src/SnapshotTests.elm
@@ -7,7 +7,10 @@ import Permutations exposing (test)
 all =
     [ test "example" <|
         \() ->
-            Permutations.verifyCombinations3 (\name b c -> GildedRose.updateQuality [ GildedRose.Item name b c ])
+            Permutations.verifyCombinations3
+                (\name b c ->
+                    GildedRose.updateQuality [ GildedRose.Item name b c ]
+                )
                 [ "", "Sulfuras, Hand of Ragnaros" ]
                 [ 1 ]
                 [ 0 ]
