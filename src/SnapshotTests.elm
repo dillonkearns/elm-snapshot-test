6ec02ad7e8b1c4e1c78801d4dacb318dd05858b8
Dillon Kearns <dillon@dillonkearns.com>
2020-08-31 15:39:42 -0700

    Extract let.

--- src/SnapshotTests.elm
+++ src/SnapshotTests.elm
@@ -9,7 +9,11 @@ all =
         \() ->
             Permutations.verifyCombinations3
                 (\name sellIn quality ->
-                    GildedRose.updateQuality [ GildedRose.Item name sellIn quality ]
+                    let
+                        item =
+                            GildedRose.Item name sellIn quality
+                    in
+                    GildedRose.updateQuality [ item ]
                 )
                 [ "", "Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" ]
                 (List.range -1 50)
